30a893780f2a410f75b31c39e73ded9cbd5763ae
Ashvini379 <ashii32mahajan@gmail.com>
2022-02-17 09:04:58 +0530

    Add project files.

---
+++ AzureWebAppConfiguration/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureWebAppConfiguration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
